12afaf8a1e4c1cbb8752d7c0df2be18f08dfa4ff
igortrabaquini <elbigodensio@gmail.com>
2026-06-09 06:08:39 -0300

    Refactor transaction generator to improve clarity

--- Fraudes/src/main/java/para/GeradorTransacoes.java
+++ Fraudes/src/main/java/para/GeradorTransacoes.java
@@ -1,36 +1,70 @@
 package para;
+
 import java.util.ArrayList;
 import java.util.Random;
 
 public class GeradorTransacoes {
 
-    public static void gerador(ArrayList<Transacao> transacao, int quantidade){
+    public static void gerador(ArrayList<Transacao> transacoes, int quantidade) {
+
         Random random = new Random();
 
-        for(int i = 0; i < quantidade-1; i++){
+        // Gera transações aleatórias
+       
+        for (int i = 0; i < quantidade - 1; i++) {
+
             StringBuilder cpf = new StringBuilder();
 
-            //Fraudes
-            if(random.nextInt(20) == 0){
+           
+            if (random.nextInt(19) == 0) {
+
+              
                 cpf.append("000");
-                for(int j = 0; j < 8; j++){
+
+               
+                for (int j = 0; j < 8; j++) {
                     cpf.append(random.nextInt(10));
                 }
 
-                transacao.add(new Transacao(String.valueOf(i + 1), (random.nextDouble(10000) + 5000), cpf.toString()));
+                
+                transacoes.add(
+                        new Transacao(
+                                String.valueOf(i + 1),
+                                random.nextDouble(10000) + 5000,
+                                cpf.toString()
+                        )
+                );
 
-            }
+            } else {
 
-            //Não fraudes
-            else {
+                // Gera um CPF comum com 11 dígitos
                 for (int j = 0; j < 11; j++) {
-                    int digito = random.nextInt(10);
+                    cpf.append(random.nextInt(10));
                 }
-                transacao.add(new Transacao(String.valueOf(i + 1), random.nextDouble(100000), cpf.toString()));
+
+              
+                if (cpf.toString().startsWith("000")) {
+                    cpf.replace(0, 3, "111");
+                }
+
+                // Gera uma transação normal
+                transacoes.add(
+                        new Transacao(
+                                String.valueOf(i + 1),
+                                random.nextDouble(100000),
+                                cpf.toString()
+                        )
+                );
             }
         }
 
-        //Garantia que havera uma fraude
-        transacao.add(new Transacao(String.valueOf(quantidade),7250, "00000000000"));
+        
+        transacoes.add(
+                new Transacao(
+                        String.valueOf(quantidade),
+                        7250,
+                        "00000000000"
+                )
+        );
     }
 }
